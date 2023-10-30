d27906dc3ceb38db2e8eaafe6e45a62cfa1a201a
Simon Glass <sjg@chromium.org>
2023-10-30 16:17:34 +1300

    wip

--- fit_validate.py
+++ fit_validate.py
@@ -4,19 +4,51 @@
 # Written by Simon Glass <sjg@chromium.org>
 
 import argparse
+import sys
+
+import libfdt
 
 def parse_args():
     epilog = 'Validate Flat Image Tree (FIT) files'
     parser = argparse.ArgumentParser(epilog=epilog)
-    parser.add_argument('FILES', type=str, nargs='*', help='Files to validate')
+    parser.add_argument('files', type=str, nargs='*', help='Files to validate')
     # parser.add_argument('-U', '--show-environment', action='store_true',
           # default=False, help='Show environment changes in summary')
 
     return parser.parse_args()
 
 
+class Validator:
+    """Validates Flat Image Tree files
+
+    Checks that the required nodes and properties are present, makes sure that
+    invalid nodes and properties are not present.
+
+    Properties:
+        fname (str): Filename being validated
+    """
+    def __init__(self, fname):
+        self.fname = fname
+
+    def validate(self):
+        pass
+
+
+def validate_fit(fdt):
+    check_root_props(fdt)
+
+
+def validate_file(fname):
+    with open(fname, 'rb') as inf:
+        fit = libfdt.Fdt(inf.read())
+        val = Validator(fit)
+        val.validate()
+
+
 def run_fit_validate():
-    return
+   args = parse_args()
+   for fname in args.files:
+       validate_file(fname)
 
 
 if __name__ == "__main__":
